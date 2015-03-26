2dc4c0b66062ee687f3523dc4b74956c9e3ea3f4
Bryan-Kirk Reinhardt <composer22@hotmail.com>
2015-03-26 09:17:09 -0700

    Revert back to original scripting

--- travis/coveralls-script.sh
+++ travis/coveralls-script.sh
@@ -1,20 +1,16 @@
 #!/bin/bash -e
-#echo "mode: count" > acc.out
-#for Dir in $(find ./* -maxdepth 10 -type d | grep -v test);
-#do
-#        if ls $Dir/*.go &> /dev/null;
-#        then
-#            go test -v -covermode=count -coverprofile=profile.out $Dir
-#            if [ -f profile.out ]
-#            then
-#                cat profile.out | grep -v "mode: count" >> acc.out
-#            fi
-#fi
-#done
-#$HOME/gopath/bin/goveralls -coverprofile=acc.out $COVERALLS_TOKEN
-#rm -rf ./profile.out
-#rm -rf ./acc.out
-echo "mode: count" > coverage.out
-go test -v -covermode=count -coverprofile=coverage.out
-$HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
-rm -rf coverage.out
+echo "mode: count" > acc.out
+for Dir in $(find ./* -maxdepth 10 -type d | grep -v test);
+do
+        if ls $Dir/*.go &> /dev/null;
+        then
+            go test -v -covermode=count -coverprofile=profile.out $Dir
+            if [ -f profile.out ]
+            then
+                cat profile.out | grep -v "mode: count" >> acc.out
+            fi
+fi
+done
+$HOME/gopath/bin/goveralls -coverprofile=acc.out $COVERALLS_TOKEN
+rm -rf ./profile.out
+rm -rf ./acc.out
