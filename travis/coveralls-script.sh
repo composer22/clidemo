dcf124eab34a223931318fc8e20c0a2eebbf7b88
Bryan-Kirk Reinhardt <composer22@hotmail.com>
2015-03-26 08:57:35 -0700

    Playing with coverage scripts

--- travis/coveralls-script.sh
+++ travis/coveralls-script.sh
@@ -1,16 +1,19 @@
 #!/bin/bash -e
-echo "mode: count" > acc.out
-for Dir in $(find ./* -maxdepth 10 -type d | grep -v test);
-do
-        if ls $Dir/*.go &> /dev/null;
-        then
-            go test -v -covermode=count -coverprofile=profile.out $Dir
-            if [ -f profile.out ]
-            then
-                cat profile.out | grep -v "mode: count" >> acc.out
-            fi
-fi
-done
-$HOME/gopath/bin/goveralls -coverprofile=acc.out $COVERALLS_TOKEN
-rm -rf ./profile.out
-rm -rf ./acc.out
+#echo "mode: count" > acc.out
+#for Dir in $(find ./* -maxdepth 10 -type d | grep -v test);
+#do
+#        if ls $Dir/*.go &> /dev/null;
+#        then
+#            go test -v -covermode=count -coverprofile=profile.out $Dir
+#            if [ -f profile.out ]
+#            then
+#                cat profile.out | grep -v "mode: count" >> acc.out
+#            fi
+#fi
+#done
+#$HOME/gopath/bin/goveralls -coverprofile=acc.out $COVERALLS_TOKEN
+#rm -rf ./profile.out
+#rm -rf ./acc.out
+
+go test -v -covermode=count -coverprofile=coverage.out
+$HOME/gopath/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
